c2d4bd9bcaf9e70be37add6ef1da8bfa56d0f3b5
Shubham Kumar <shubhamkyp300@gmail.com>
2026-06-20 14:06:58 +0530

    Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is up to date with 'origin/main'.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,34 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
-const PORT = process.env.PORT;
-const app = express();
+import dns from "dns";
+import { clerkMiddleware } from "@clerk/express";
 
-app.listen(PORT, () => {
-  console.log("Server is running on port number is", PORT);
+import { connectDB } from "./lib/db.js";
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
+// Middlewares
+app.use(express.json());
+app.use(clerkMiddleware());
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+    credentials: true,
+  }),
+);
+
+// Routes
+app.get("/health", (req, res) => {
+  res.status(200).json({ ok: true });
+});
+
+// Server
+app.listen(PORT, async () => {
+  await connectDB();
+  console.log(`Server is up and running on PORT: ${PORT}`);
 });
