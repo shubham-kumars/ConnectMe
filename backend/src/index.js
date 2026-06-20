dfa0c9044cdbf6cce9e6bcd404a1c59487c37ad3
Shubham Kumar <shubhamkyp300@gmail.com>
2026-06-20 17:38:23 +0530

    Add Dockerfile and .dockerignore; enhance backend to serve static files

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
-import "dotenv/config";
 import dns from "dns";
+import "dotenv/config";
+import fs from "fs";
+import path from "path";
 import { clerkMiddleware } from "@clerk/express";
 
 import { connectDB } from "./lib/db.js";
@@ -11,6 +13,15 @@ dns.setServers(["1.1.1.1", "8.8.8.8"]);
 const app = express();
 const PORT = process.env.PORT || 5000;
 const FRONTEND_URL = process.env.FRONTEND_URL;
+
+const publicDir = path.join(process.cwd(), "public");
+
+// it's important that you don't parse the webhook event data, it should be in the raw format
+app.use(
+  "/api/webhooks/clerk",
+  express.raw({ type: "application/json" }),
+  clerkWebhook,
+);
 
 // Middlewares
 app.use(express.json());
@@ -26,6 +37,18 @@ app.use(
 app.get("/health", (req, res) => {
   res.status(200).json({ ok: true });
 });
+
+
+// if the public directory exists, serve the static files
+// this is for the production build
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
+}
+
 
 // Server
 app.listen(PORT, async () => {
